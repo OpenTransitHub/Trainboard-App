c416e3aed39fbbda22abfa93554f4723859ad017
Felix <felix-nietzold@outlook.de>
2025-10-09 19:34:32 +0200

    Add random wallpapers

--- assets/src/backgroundimage.js
+++ assets/src/backgroundimage.js
@@ -1,24 +1,33 @@
 if (localStorage.getItem("selectedWallpaper") !== null) {
-    document.body.style.backgroundImage = `url('./assets/wallpapers/${localStorage.getItem("selectedWallpaper")}.jpg')`;
+
+    let imgname = localStorage.getItem("selectedWallpaper");
+
+    if (localStorage.getItem("randomimg") === 'true') {
+      imgname = 'wallpaper' + (Math.floor(Math.random() * 5) + 1);
+    }
+
+    document.body.style.backgroundImage = `url('./assets/wallpapers/${imgname}.jpg')`;
   
-    if (localStorage.getItem("selectedWallpaper") === 'wallpaper1') {
+    if (imgname === 'wallpaper1') {
       document.body.style.backgroundPositionX = 'center';
     }
 
-   if (localStorage.getItem("selectedWallpaper") === 'wallpaper2') {
+   if (imgname === 'wallpaper2') {
       document.body.style.backgroundPositionY = 'bottom';
       document.body.style.backgroundPositionX = '30%';
     }
 
-    if (localStorage.getItem("selectedWallpaper") === 'wallpaper3') {
+    if (imgname === 'wallpaper3') {
       document.body.style.backgroundPositionX = 'center';
     }
 
-    if (localStorage.getItem("selectedWallpaper") === 'wallpaper5') {
+    if (imgname === 'wallpaper5') {
       document.body.style.backgroundPositionY = '100%';
       document.body.style.backgroundPositionX = '35%';
 
     }
+
+    
 
 } else {
     document.body.style.backgroundImage = `url('./assets/wallpapers/wallpaper1.jpg')`;
