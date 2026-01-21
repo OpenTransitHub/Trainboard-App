af42f6b4084ca518c98a3e0569cd59f9dbb262e4
Felix <felix-nietzold@outlook.de>
2026-01-21 18:32:29 +0100

    Removed Provider loop

--- assets/src/providersettings.js
+++ assets/src/providersettings.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const providerKey = "provider";
-    let providerData = localStorage.getItem(providerKey);
-    let provider;
 
-    // 1. Provider-Daten laden oder Initialisieren
-    if (!providerData) {
-        provider = {
-            providerName: "Deutsche Bahn",
-            providerID: "de",
-            regionDisplay: "de" // Der Zielordnername
-        };
-        localStorage.setItem(providerKey, JSON.stringify(provider));
-    } else {
-        provider = JSON.parse(providerData);
-    }
+    const providerCheck = () => {
+        let providerData = localStorage.getItem(providerKey);
+        let provider;
 
-    // 2. Den aktuellen Ordnernamen ermitteln
-    // Beispiel: bei "/de/index.html" liefert dies "de"
-    const pathSegments = window.location.pathname.split('/').filter(segment => segment !== "");
-    const currentFolder = pathSegments[pathSegments.length - 2];
+        if (!providerData) {
+            provider = {
+                providerName: "Deutsche Bahn",
+                providerID: "db",
+                regionDisplay: "de" 
+            };
+            localStorage.setItem(providerKey, JSON.stringify(provider));
+        } else {
+            provider = JSON.parse(providerData);
+        }
 
-    // 3. Logik: Wenn aktueller Ordner NICHT regionDisplay entspricht -> Redirect
-    if (currentFolder !== provider.regionDisplay) {
-        window.location.href = `../${provider.regionDisplay}/index.html`;
-    }
+        const pathSegments = window.location.pathname.split('/').filter(segment => segment !== "");
+        const currentFolder = pathSegments[pathSegments.length - 2];
+        
+        if (currentFolder !== provider.regionDisplay.toLocaleLowerCase()) {
+            window.location.href = `../${provider.regionDisplay}/index.html`;
+        }
+    };
+
+    providerCheck();
+
+    setInterval(providerCheck, 1000); 
 });
